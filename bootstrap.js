098f8cb4da8610f0ec2a70987ddc19cfb70183e8
Andreas Streichardt <andreas.streichardt@gmail.com>
2015-09-27 22:53:20 +0200

    Import zone.js separately ... fixing interpolation

--- bootstrap.js
+++ bootstrap.js
@@ -1,5 +1,7 @@
 // mop: angular2 needs the reflect shim to be loaded
 import Reflect from 'node_modules/angular2/node_modules/reflect-metadata/Reflect.js';
+// mop: without zone.js interpolation won't work (?)
+import Zone from 'node_modules/angular2/node_modules/zone.js/lib/zone.js';
 import {bootstrap} from 'demo/demo.js';
 
 bootstrap();
